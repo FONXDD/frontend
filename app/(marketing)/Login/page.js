829c61c78f5a44afe38c2eb9e05c7fe4c8f33599
FONXDD <boyb11283@gmail.com>
2025-08-20 11:28:50 +0700

    update

--- app/(marketing)/Login/page.js
+++ app/(marketing)/Login/page.js
@@ -12,7 +12,7 @@ export default function Page() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://itdev.cmtc.ac.th:3000/api/auth/login', {
+      const res = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
